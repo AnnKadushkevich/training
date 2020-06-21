13d09b063eb213eec0dbcbbc4ab59b87e27e7bd6
AnnaKad <annakozlovskay23@gmail.com>
2020-06-21 23:47:01 +0300

    тест добавления контакта

---
+++ test_add_contacts.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from group_for_contacts import Group
+import unittest
+
+
+class UntitledTestCase ( unittest.TestCase ):
+    def setUp(self):
+        self.wd = webdriver.Firefox ()
+        self.wd.implicitly_wait ( 30 )
+
+    def open_home_page(self, wd):
+        wd.get ( "http://localhost/addressbook/" )
+
+    def login(self, wd, username="admin", password="secret"):
+        wd.find_element_by_name ( "user" ).clear ()
+        wd.find_element_by_name ( "user" ).send_keys ( username )
+        wd.find_element_by_name ( "pass" ).click ()
+        wd.find_element_by_name ( "pass" ).clear ()
+        wd.find_element_by_name ( "pass" ).send_keys ( password )
+        wd.find_element_by_id ( "LoginForm" ).submit ()
+
+    def add_contact(self, wd, group ):
+        # init contact creation
+        wd.find_element_by_link_text ( "add new" ).click ()
+        # fill contact form
+        wd.find_element_by_name ( "firstname" ).click ()
+        wd.find_element_by_name ( "firstname" ).clear ()
+        wd.find_element_by_name ( "firstname" ).send_keys ( group.firstname )
+        wd.find_element_by_name ( "middlename" ).click ()
+        wd.find_element_by_name ( "middlename" ).clear ()
+        wd.find_element_by_name ( "middlename" ).send_keys ( group.middlename )
+        wd.find_element_by_name ( "lastname" ).click ()
+        wd.find_element_by_name ( "lastname" ).clear ()
+        wd.find_element_by_name ( "lastname" ).send_keys (group.lastname )
+        wd.find_element_by_name ( "nickname" ).click ()
+        wd.find_element_by_name ( "nickname" ).clear ()
+        wd.find_element_by_name ( "nickname" ).send_keys ( group.nickname )
+        wd.find_element_by_name ( "title" ).click ()
+        wd.find_element_by_name ( "title" ).clear ()
+        wd.find_element_by_name ( "title" ).send_keys ( group.titile )
+        wd.find_element_by_name ( "company" ).click ()
+        wd.find_element_by_name ( "company" ).clear ()
+        wd.find_element_by_name ( "company" ).send_keys ( group.compony )
+        wd.find_element_by_name ( "address" ).click ()
+        wd.find_element_by_name ( "address" ).clear ()
+        wd.find_element_by_name ( "address" ).send_keys ( group.address )
+        wd.find_element_by_name ( "home" ).click ()
+        wd.find_element_by_name ( "home" ).clear ()
+        wd.find_element_by_name ( "home" ).send_keys ( group.home )
+        wd.find_element_by_name ( "mobile" ).click ()
+        wd.find_element_by_name ( "mobile" ).clear ()
+        wd.find_element_by_name ( "mobile" ).send_keys ( group.mobile )
+        wd.find_element_by_name ( "work" ).click ()
+        wd.find_element_by_name ( "work" ).clear ()
+        wd.find_element_by_name ( "work" ).send_keys ( group.work )
+        wd.find_element_by_name ( "fax" ).click ()
+        wd.find_element_by_name ( "fax" ).clear ()
+        wd.find_element_by_name ( "fax" ).send_keys (group.fax )
+        wd.find_element_by_name ( "email" ).click ()
+        wd.find_element_by_name ( "email" ).clear ()
+        wd.find_element_by_name ( "email" ).send_keys ( group.email )
+        wd.find_element_by_name ( "email2" ).click ()
+        wd.find_element_by_name ( "email2" ).clear ()
+        wd.find_element_by_name ( "email2" ).send_keys ( group.email2 )
+        wd.find_element_by_name ( "email3" ).click ()
+        wd.find_element_by_name ( "email3" ).clear ()
+        wd.find_element_by_name ( "email3" ).send_keys ( group.email3 )
+        wd.find_element_by_name ( "homepage" ).click ()
+        wd.find_element_by_name ( "homepage" ).clear ()
+        wd.find_element_by_name ( "homepage" ).send_keys ( group.homepage )
+        wd.find_element_by_name ( "bday" ).click ()
+        Select ( wd.find_element_by_name ( "bday" ) ).select_by_visible_text ( "14" )
+        wd.find_element_by_name ( "bday" ).click ()
+        wd.find_element_by_name ( "bmonth" ).click ()
+        Select ( wd.find_element_by_name ( "bmonth" ) ).select_by_visible_text ( "August" )
+        wd.find_element_by_name ( "bmonth" ).click ()
+        wd.find_element_by_name ( "byear" ).click ()
+        wd.find_element_by_name ( "byear" ).clear ()
+        wd.find_element_by_name ( "byear" ).send_keys ( "1955" )
+        wd.find_element_by_name ( "aday" ).click ()
+        Select ( wd.find_element_by_name ( "aday" ) ).select_by_visible_text ( "18" )
+        wd.find_element_by_name ( "aday" ).click ()
+        wd.find_element_by_name ( "amonth" ).click ()
+        Select ( wd.find_element_by_name ( "amonth" ) ).select_by_visible_text ( "October" )
+        wd.find_element_by_name ( "amonth" ).click ()
+        wd.find_element_by_name ( "ayear" ).click ()
+        wd.find_element_by_name ( "ayear" ).clear ()
+        wd.find_element_by_name ( "ayear" ).send_keys ( "2012" )
+        wd.find_element_by_name ( "address2" ).click ()
+        wd.find_element_by_name ( "address2" ).clear ()
+        wd.find_element_by_name ( "address2" ).send_keys ( group.address2 )
+        wd.find_element_by_name ( "phone2" ).click ()
+        wd.find_element_by_name ( "phone2" ).clear ()
+        wd.find_element_by_name ( "phone2" ).send_keys (group.phone2 )
+        wd.find_element_by_name ( "notes" ).click ()
+        wd.find_element_by_name ( "notes" ).clear ()
+        wd.find_element_by_name ( "notes" ).send_keys ( group.notes )
+        # submit group creation
+        wd.find_element_by_xpath ( "(//input[@name='submit'])[2]" ).click ()
+
+    def retern_to_home_page(self, wd):
+        wd.find_element_by_link_text ( "home page" ).click ()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text ( "Logout" ).click ()
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page ( wd )
+        self.login ( wd, username="admin", password="secret" )
+        self.add_contact ( wd , Group(firstname = 'anna',middlename = ' alex',
+            lastname = ' koz',nickname = ' an ',title = 'no',company = 'no',
+            address = 'min',home='none',mobile = ' 35661',work = 'not',
+            fax = ' 64131ftbr',email = ' uhui46@iby',email2='iuhi@jiyg',email3 = 'gugu@545trb',
+            homepage = 'yuuh ',address2='ugy',phone2 = 'ugu',notes = 'ukhi'))
+        self.retern_to_home_page ( wd )
+        self.logout ( wd )
+
+    def test_add_empty_contact(self):
+        wd = self.wd
+        self.open_home_page ( wd )
+        self.login (wd, username="admin", password="secret" )
+        self.add_contact ( wd, Group(firstname = '',middlename='',
+            lastname='',nickname='',title='',company='',
+            address='',home='',mobile='',work='',
+            fax='',email='',email2='',email3='',
+            homepage='',address2='',phone2='',notes='') )
+        self.retern_to_home_page ( wd )
+        self.logout ( wd )
+
+    def tearDown(self):
+        self.wd.quit ()
+
+
+if __name__ == "__main__":
+    unittest.main ()
