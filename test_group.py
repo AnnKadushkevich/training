d58d4021c9e749f768dd194206fbde600c33fdfb
AnnaKad <annakozlovskay23@gmail.com>
2020-07-02 21:12:25 +0300

    удален дублирующий тест

--- test_group.py
+++ test_group.py
@@ -13,8 +13,3 @@ def test_add_group(app):
     app.login(username="admin", password ="secret")
     app.create_group(Group(name="first group", header="hjklouhi", footer="lhb"))
     app.logout()
-
-def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
-    app.logout()
