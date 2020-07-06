1ac4eabd45c36165d0966bbd187eb96f2f57abd1
AnnaKad <annakozlovskay23@gmail.com>
2020-07-06 23:39:54 +0300

    созданы тесты для редактирования контактов

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -1,5 +1,5 @@
 
-def test_edit_contact(self):
+def test_edit_contact(app):
     app.session.login ( username="admin", password="secret" )
     app.contacts.edit_contact()
     app.session.logout ()
