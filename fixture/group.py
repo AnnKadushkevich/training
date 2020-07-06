b758f2f8486f137f73e9be7ca3dc79f53b1c58b6
AnnaKad <annakozlovskay23@gmail.com>
2020-07-06 20:57:07 +0300

    создан метод для удаления группы, фикстура сделана общей scope="session"

--- fixture/group.py
+++ fixture/group.py
@@ -28,6 +28,16 @@ class GroupHelper:
         wd.find_element_by_name("submit" ).click()
         self.app.retern_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.app.open_home_page ()
+        #select first group
+        wd.find_element_by_name ( "selected[]" ).click ()
+        #submit deletion
+        wd.find_element_by_name ( "delete" ).click()
+        self.app.retern_to_groups_page ()
+
+
     def retern_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
