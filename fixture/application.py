f5dff5504fd35bbd2c2ad3936a81d16ef8155052
AnnaKad <annakozlovskay23@gmail.com>
2020-07-06 19:23:22 +0300

    в файле application.py инициализирован ContactHelper

--- fixture/application.py
+++ fixture/application.py
@@ -8,6 +8,7 @@ class Application:
         self.wd.implicitly_wait ( 30 )
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contacts = ContactHelper(self)
 
 
     def open_home_page(self):
