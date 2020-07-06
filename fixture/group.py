8d1f13668fcb6a320adc7c762fbb92a0fb285a7d
AnnaKad <annakozlovskay23@gmail.com>
2020-07-06 21:37:56 +0300

    создан метод для удаления и редактирования группы

--- fixture/group.py
+++ fixture/group.py
@@ -37,6 +37,24 @@ class GroupHelper:
         wd.find_element_by_name ( "delete" ).click()
         self.app.retern_to_groups_page ()
 
+    def edit_group(self):
+        wd = self.app.wd
+        self.app.open_home_page ()
+        # select group
+        wd.find_element_by_name ( "selected[]" ).click ()
+        # editing
+        wd.find_element_by_name ( "Edet group" ).click ()
+        wd.find_element_by_name ( "group_name" ).click ()
+        wd.find_element_by_name ( "group_name" ).clear ()
+        wd.find_element_by_name ( "group_name" ).send_keys ( "koolh" )
+        wd.find_element_by_name ( "group_header" ).click ()
+        wd.find_element_by_name ( "group_header" ).clear ()
+        wd.find_element_by_name ( "group_header" ).send_keys ( "lfgolsfpg" )
+        wd.find_element_by_name ( "group_footer" ).click ()
+        wd.find_element_by_name ( "group_footer" ).clear ()
+        wd.find_element_by_name ( "group_footer" ).send_keys ( "klfkjp" )
+        wd.find_element_by_name ( "update" ).click ()
+        self.app.retern_to_groups_page ()
 
     def retern_to_groups_page(self):
         wd = self.app.wd
