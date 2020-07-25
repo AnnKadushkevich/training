84119bb34ba79a759d2627d24d8224ed46372d49
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-07-25 22:05:02 +0300

    Update test_del_contact.py

--- tests/test_del_contact.py
+++ tests/test_del_contact.py
@@ -4,6 +4,9 @@ from model.contact import Contact
 def test_delete_contact(app):
     if app.contacts.count() == 0:
         app.contacts.create(Contact(firstname='test'))
-
+    old_contact = app.contacts.get_contact_list ()
     app.contacts.delete_first_contact()
-
+    new_contact = app.contacts.get_contact_list ()
+    assert len ( old_contact - 1 ) == len ( new_contact )
+    old_contact[0:1] = []
+    assert old_contact == new_contact
