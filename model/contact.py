37ae46bdfe7f94f11cbee727b593254250cbc899
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-07-25 22:01:54 +0300

    Update contact.py

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,4 @@
+from sys import maxsize
 class Contact:
 
     def __init__(
@@ -6,7 +7,7 @@ class Contact:
             address,home,mobile,work,
             fax,email,email2,email3,
             homepage,address2,phone2,
-            notes
+            notes, id
     ):
         self.firstname = firstname
         self.middlename = middlename
@@ -26,3 +27,16 @@ class Contact:
         self.address2 = address2
         self.phone2 = phone2
         self.notes = notes
+        self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.firstname, self.lastname)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
+
+    def id_or_max(self):
+        if self.id:
+            return int ( self.id )
+        else:
+            return maxsize
