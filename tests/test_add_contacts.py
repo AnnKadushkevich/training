a55cc1153c193bc637909b1668d317b3164e250d
Ann <66567502+AnnKadushkevich@users.noreply.github.com>
2020-07-25 22:03:21 +0300

    Update test_add_contacts.py

--- tests/test_add_contacts.py
+++ tests/test_add_contacts.py
@@ -4,13 +4,16 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-
-    app.contact.create( Contact( firstname ='anna', middlename =' alex',
+    old_contact = app.contacts.get_contact_list ()
+    contact =  Contact( firstname ='anna', middlename =' alex',
                                  lastname = ' koz', nickname = ' an ', title = 'no', company = 'no',
                                  address = 'min', home='none', mobile = ' 35661', work = 'not',
                                  fax = ' 64131ftbr', email = ' uhui46@iby', email2='iuhi@jiyg', email3 = 'gugu@545trb',
-                                 homepage = 'yuuh ', address2='ugy', phone2 = 'ugu', notes = 'ukhi' ) )
-
-
+                                 homepage = 'yuuh ', address2='ugy', phone2 = 'ugu', notes = 'ukhi' )
+    app.contact.create( contact )
+    new_contact = app.contacts.get_contact_list ()
+    assert len ( old_contact + 1 ) == len ( new_contact )
+    old_contact.append ( contact )
+    assert sorted ( old_contact, key=Contact.id_or_max ) == sorted ( new_contact, key=Contact.id_or_max )
 
 
